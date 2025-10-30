f8c0b534f01e6bdf4e11fe1e7d09c64fa5d84b1f
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-10-30 02:00:02 +0000

    feat: refactor clarity script integration to use next/script component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
-import { Poppins } from "next/font/google";
 import "./globals.css";
+import Script from 'next/script';
+import { Poppins } from "next/font/google";
 import Header from "@/components/common/Header";
 import Footer from "@/components/common/Footer";
 import Quotes from "@/components/common/Quotes";
@@ -15,20 +16,10 @@ export const metadata = {
   description: "Personal website of Dhiraj Arya, a self-taught developer",
 };
 
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
-<head>
-<!-- clerity code -->
-<script type="text/javascript">
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "ty0etl4nzb");
-</script>
-
-</head>
       <body className={`${poppins.className} antialiased`}>
         <div className="w-full mx-auto max-w-3xl scroll-smooth relative">
           <Header />
@@ -36,6 +27,15 @@ export default function RootLayout({ children }) {
           <Quotes />
           <Footer />
         </div>
+        {/* clarity code  */}
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`(function(c,l,a,r,i,t,y){
+            c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "ty0etl4nzb");
+          `}
+        </Script>
       </body>
     </html>
   );
