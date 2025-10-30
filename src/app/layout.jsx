88a90c55ac9fd0ac7b5afbf79f4963230b681e16
Dhiraj Kumar <dhirajarya.ptn@gmail.com>
2025-10-30 06:04:52 +0530

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -18,6 +18,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
+<head>
+<!-- clerity code -->
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "ty0etl4nzb");
+</script>
+
+</head>
       <body className={`${poppins.className} antialiased`}>
         <div className="w-full mx-auto max-w-3xl scroll-smooth relative">
           <Header />
