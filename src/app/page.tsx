e24984200ab92038f2148e65a81d5d4d9eaccd8e
Dhiraj Arya <dhirajarya.ptn@gamil.com>
2025-12-31 11:02:04 +0000

    fix: adjust grid classes for responsive project layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
           hintText={"my proof-of-work✨"}
           className={"mt-18"}
         >
-          <div className="grid  grid-cols-1 sm:grid-cols-2 gap-4 mt-4">
+          <div className="grid grid-cols-1 min-[600px]:grid-cols-2  gap-4 mt-4">
             {projects.map((project) => (
               <ProjectCard key={project.slug} project={project} />
             ))}
