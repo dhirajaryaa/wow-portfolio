e3497f21f46e0e7ae2a72107c8724d1979c11943
Dhiraj Arya <dhirajarya.ptn@gamil.com>
2026-01-04 16:34:24 +0000

    feat: integrate react-hook-form and zod for form validation in ContactForm; update schema and handle form submission errors

--- src/action/contact.ts
+++ src/action/contact.ts
@@ -1,6 +1,8 @@
 "use server";
 
-import { NextRequest } from "next/server";
+import ContactForm, { ContactFormProps } from "@/components/contact/ContactForm";
+import { ContactSchema } from "@/lib/schema";
+import z from "zod";
 
 interface Response {
     success: boolean;
@@ -13,21 +15,30 @@ const telegramChatId = process.env.TELEGRAM_CHAT_ID;
 const telegramApiUrl = `https://api.telegram.org/bot${telegramBotToken}/sendMessage`;
 
 
-export const sendMessage = async (formData: FormData): Promise<Response> => {
+export const sendMessage = async (data: ContactFormProps): Promise<Response> => {
+    // check data 
+    const valid = ContactSchema.safeParse(data);
+    if (!valid.success) {
+        return {
+            success: false,
+            message: "Something went wrong. Please try again.⚠️",
+            error: z.prettifyError(valid.error),
+        };
+    };
 
     // parse message 
     const message = `
 📩 *New Contact Enquiry*
 
-👤 *Name:* ${formData.get("name")}
-📞 *Phone:* ${formData.get("phone")}
-📧 *Email:* ${formData.get("email")}
+👤 *Name:* ${data.name}
+📞 *Phone:* ${data.phone}
+📧 *Email:* ${data.email}
 
 💬 *Message:*
-${formData.get("message").toString().trim()}
+${data.message.trim()}
 
 -----------------------------------------------
-🗓️ *Submitted:* ${formData.get("submitted")}
+🗓️ *Submitted:* ${data.submitted}
 `;
 
     const response = await fetch(telegramApiUrl, {
