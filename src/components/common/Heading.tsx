eeb002b9c4e8635a54ff30fc5ee421e977841fdf
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2026-04-30 18:22:01 +0530

    feat: replace DevMessage with Heading component for project section and enhance Heading styling

--- src/components/common/Heading.tsx
+++ src/components/common/Heading.tsx
@@ -7,7 +7,7 @@ interface HeadingProps {
 
 function Heading({ title, description }: HeadingProps): React.ReactElement {
   return (
-    <section className="w-full text-center py-16 border-b">
+    <section className="w-full text-center sm:py-16 py-10 border-b space-y-1">
       <h1 className="text-4xl sm:text-5xl font-semibold">{title}</h1>
       <p className="text-muted-foreground text-sm sm:text-lg">{description}</p>
     </section>
